9a7ea1ed1617264df890867e94d638669b9c18b1
jinju9513 <wjswlswn99@naver.com>
2025-04-03 09:54:03 +0900

    first commit

---
+++ bin/daily_algorithm.dart
@@ -0,0 +1,5 @@
+import 'package:daily_algorithm/daily_algorithm.dart' as daily_algorithm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${daily_algorithm.calculate()}!');
+}
---
+++ test/daily_algorithm_test.dart
@@ -0,0 +1,8 @@
+import 'package:daily_algorithm/daily_algorithm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
